69d79af0b33cb176dd54c59b61f9795d040710c6
NanuIhsNenhs <13213058939@163.com>
2021-02-17 18:51:05 +0800

    second

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 const app = Vue.createApp({
     data() {
         return {
-            product:'beautiful girls'
+            product1:'beautiful',
+            product2:'girls',
+            cart:0,
+            // image1:'./assets/images/01.jpg',
+            // image2:'./assets/images/02.jpg',
+            orimage:'./assets/images/xp.jpg',
+            image:'./assets/images/xp.jpg',
+            inStock:true,
+            details:['jiushi','yangchen','luxuan'],
+            variants:[
+                {id:2233,posture:'sit',image:'./assets/images/01.jpg'},
+                {id:2333,posture:'kneel',image:'./assets/images/02.jpg'}
+            ],
         }
     },
-})
+    methods: {
+        addToCart(){
+            this.cart+=1
+        },
+        updateImage(cimage){
+            this.image=cimage
+            console.log('update',this.image);
+        }
+    },
+    mounted() {
+        setInterval(()=>{
+            this.inStock=!this.inStock
+            // this.cart++
+        },2000)
+    },
+}).mount('#app')
