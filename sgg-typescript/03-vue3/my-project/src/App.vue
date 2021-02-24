1197564bfb869ce844f6885d12980c03e1d49879
NauyIhsNehs <13213058939@163.com>
2021-02-24 18:47:29 +0800

    响应式原理-基操

--- sgg-typescript/03-vue3/my-project/src/App.vue
+++ sgg-typescript/03-vue3/my-project/src/App.vue
@@ -1,25 +1,55 @@
 <template>
   <h2>{{ count }}</h2>
   <div>哈哈哈</div>
-  <button @click="update">update</button>
+  <button @click="updateref">update1</button>
+  <button @click="updatereactive">update2</button>
+  <hr />
+  <Child :msg="cmsg" msg2="真香" @XXX="xxx"></Child>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
-
+import { defineComponent, reactive, ref } from "vue";
+import Child from "./components/child.vue";
 export default defineComponent({
-  setup() {
+  name: "App",
+  components: {
+    Child,
+  },
+  setup(props, context) {
     //定义响应式对象
+    const obj: any = {
+      //注意any，否则不能响应式添加数据
+      name: "nauyihsnehs",
+      age: 21,
+      gender: "male",
+    };
     const count = ref(1);
+    const cmsg = ref("nishiwoxinzhongzuimeideyuncai");
+    // const user = reactive<any>(obj); //注意any，否则不能响应式添加数据
+    const user = reactive(obj);
     console.log(count);
+    console.log(user);
 
-    function update() {
+    function updateref() {
       count.value++;
     }
 
+    const updatereactive = () => {
+      user.name += "...";
+      user.age--;
+      user.handsome = "true";
+      console.log(user);
+    };
+    const xxx = (str: string) => {
+      cmsg.value += str;
+    };
     return {
       count,
-      update,
+      user,
+      updateref,
+      updatereactive,
+      cmsg,
+      xxx,
     };
   },
 });
---
+++ sgg-typescript/03-vue3/my-project/src/components/child.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>I'm child components {{ msg }}</h1>
+  <button @click="emitXXX">button</button>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Child",
+  props: ["msg"],
+  setup(props, context) {
+    // 在beforeCreate前执行
+    // props: 包含props配置声明且传入了的所有属性的对象
+    // attrs: 包含没有在props配置中声明的属性的对象, 相当于 this.$attrs
+    // slots: 包含所有传入的插槽内容的对象, 相当于 this.$slots
+    // emit: 用来分发自定义事件的函数, 相当于 this.$emit
+    // context包括attrs、slots、emit等
+    console.log(props);
+    console.log(context.attrs);
+    console.log(context.attrs.msg2);
+    console.log(context.emit);
+
+    const emitXXX = () => {
+      context.emit("XXX", "++");
+    };
+
+    return {
+      emitXXX,
+    };
+  },
+});
+</script>
